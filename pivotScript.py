00111c6fbcc361f1e2b293252cc2385a9a1ccea0
John Min <jcm2199@columbia.edu>
2013-11-24 05:50:29 -0500

    first commit of pivotScript

---
+++ pivotScript.py
@@ -0,0 +1,16 @@
+import os
+import sys
+import pickle
+
+
+from collections import defaultdict
+
+data_dict = defaultdict(int)
+
+rootdir = sys.argv[1]
+
+for root, subFolders, files in os.walk(rootdir):
+    for folder in subFolders:
+	print str(folder)
+	for f in files:
+	    print str(f)
