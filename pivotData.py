109ba8ac8c92fbd75bf9862eeb88dc27b99add51
John Min <jcm2199@columbia.edu>
2013-11-24 06:41:48 -0500

    finalized pivotDdata

--- pivotData.py
+++ pivotData.py
@@ -2,16 +2,15 @@ import os
 import sys
 import pickle
 
-
 from collections import defaultdict
+
+
+def main(root_dir):
 
 data_dict = defaultdict(int)
 
-rootdir = sys.argv[1]
-
-for root, dirs, files in os.walk(rootdir):
+for root, dirs, files in os.walk(root_dir):
     print root
-    print dirs
     for file_name in files:
         file_path = root + '/' + file_name
         f = open(file_path, 'rb')
@@ -20,3 +19,11 @@ for root, dirs, files in os.walk(rootdir):
             uid, songid, platform, count= line.split('\x01')
             data_dict[(uid,songid)] += int(count)
 
+	pickle.dump(data_dict, open('data_dict.p', 'wb'))
+
+pickle.dump(data_dict, open('data_dict.p', 'wb'))
+
+
+if name == '__main__':
+    root_dir = sys.argv[1]
+    main(root_dir)
