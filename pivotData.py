1b28ffe49ca2f78a1c5ea7b8b7e60bdb657220af
John Min <jcm2199@columbia.edu>
2013-11-24 06:34:53 -0500

    delimiter

--- pivotData.py
+++ pivotData.py
@@ -9,10 +9,14 @@ data_dict = defaultdict(int)
 
 rootdir = sys.argv[1]
 
-for root, subFolders, files in os.walk(rootdir):
-    for folder in subFolders:
-	print str(folder)
-	'''
-	for f in files:
-	    print str(f)
-	'''
+for root, dirs, files in os.walk(rootdir):
+    print root
+    print dirs
+    for file_name in files:
+        file_path = root + '/' + file_name
+        f = open(file_path, 'rb')
+
+        for line in f:
+            uid, songid, platform, count= line.split('\x01')
+            data_dict[(uid,songid)] += int(count)
+
