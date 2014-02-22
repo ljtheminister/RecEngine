6cf186cea38ee0eef6f008b32db6a2d1bf07008a
ljtheminister <john.c.min@gmail.com>
2014-02-22 00:37:38 -0500

    binary clustering

---
+++ Binary_Clustering.py
@@ -0,0 +1,33 @@
+import cPickle as pickle
+import numpy as np
+import pandas as pd
+
+    
+def load_pickled_file(filename):
+    data = pickle.load(open(filename, 'rb'))
+    return data
+
+
+filename = 'AddQ_mat_binary.p'
+data = load_pickled_file(filename)
+
+users = []
+songs = []
+
+with open('uids.txt', 'rb') as uids:
+    for line in uids:
+	users.append(line.split()[0])
+
+with open('songids.txt', 'rb') as songids:
+    for line in songids:
+	songs.append(line.split()[0])
+
+N = len(users)
+M = len(songs)
+
+mat = np.zeros((N,M))
+mat = pd.DataFrame(mat)
+mat.index = users
+mat.columns = songs
+mat.save('mat.pd')
+
